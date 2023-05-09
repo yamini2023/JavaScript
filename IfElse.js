12a33a330593f4f6bea78a818d6c8bfcce3b325f
yamini2023 <132660020+yamini2023@users.noreply.github.com>
2023-05-09 15:05:01 +0530

    Add files via upload

---
+++ IfElse.js
@@ -0,0 +1,41 @@
+//If else
+const day=prompt("enter a week");
+if(day==1)
+{
+   console.log("Its monday");
+ 
+}
+else if(day==2)
+{
+   console.log("Its Tuesday");
+ 
+}
+else if(day==3)
+{
+   console.log("Its Wednesday");
+ 
+}
+else if(day==4)
+{
+   console.log("Its Thursday");
+ 
+}
+else if(day==5)
+{
+   console.log("Its Friday");
+ 
+}
+else if(day==6)
+{
+   console.log("Its Saturday");
+ 
+}
+else if(day==7)
+{
+   console.log("Its Sunday");
+ 
+}
+else
+{
+    console.log("Please enter a valid day");
+}
